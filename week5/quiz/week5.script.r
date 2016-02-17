39196c281495edd8a8e6dd1f2354cd0b491771f2
Zhihan Fang <zhihan.fang@rutgers.edu>
2016-02-17 00:18:21 -0500

    sync

--- week5/quiz/week5.script.r
+++ week5/quiz/week5.script.r
@@ -20,14 +20,39 @@ sorted.country[1]
 
 # calculate the difference of mean value
 sorted.country[length(sorted.country)] - sorted.country[1]
-
+two.country.happiness <-subset(happiness,happiness$COUNTRY=='Switzerland'|happiness$COUNTRY=='Togo')
 #happiness value of Switzerland
 switzerland.happiness <- happiness[happiness$COUNTRY=='Switzerland',6]
 #happiness value of Togo
 togo.happiness <- happiness[happiness$COUNTRY=='Togo',6]
 
-#mean difference
-
+#mean differences
+D <- mean(switzerland.happiness) - mean(togo.happiness)
 #data distribution
 boxplot(switzerland.happiness,togo.happiness,names = c('Switzerland','Togo'))
 
+#number of people from switzerland
+l_switzerland <- length(switzerland.happiness)
+#number of people from togo
+l_togo <- length(togo.happiness)
+l <- l_switzerland + l_togo
+
+#set null country
+null_country <- rep("Swizterland",l)
+null_country[sample(l,l_togo)] <- 'Togo'
+null <- data.frame(null_country,two.country.happiness[,6])
+null
+
+switzerland_null <-null[null$null_country=='Swizterland',2]
+togo_null <-null[null$null_country=='Togo',2]
+
+D_null <- mean(switzerland_null) - mean(togo_null)
+
+for(i in 1:10000){
+  null_country <- rep("Swizterland",l)
+  null_country[sample(l,l_togo)] <- 'Togo'
+  null <- data.frame(null_country,two.country.happiness[,6])
+  switzerland_null <-null[null$null_country=='Swizterland',2]
+  togo_null <-null[null$null_country=='Togo',2]
+  D_null[i] <- mean(switzerland_null) - mean(togo_null)
+}
