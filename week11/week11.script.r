5c591f51c3084fcdfe699f5609f9574d3d8c3eca
Zhihan <ezhihan@gmail.com>
2016-04-05 23:18:08 -0400

    week11 finished

--- week11/week11.script.r
+++ week11/week11.script.r
@@ -4,7 +4,7 @@ library(MASS)
 library(rpart)
 #import training and testing data
 
-lou_training <- read.csv("~/workspace/data_literacy/week11/lou_training.csv")
+lou_training <- read.csv("~/workspace/data_literacy/week11/SUAREZ.csv")
 
 #set fold number
 fold_number = 10
@@ -16,8 +16,8 @@ row_number_each_fold <-n_row/fold_number
 
 #vector that contains all results from validation
 error.tree <- c()
-error.lda <- c()
-error.forest <- c()
+error.tree2 <- c()
+error.tree3 <- c()
 
 # first time, set first fold as testing data; second loop, set second fold as test data, and so on.
 for (i in 1:fold_number){
@@ -35,22 +35,23 @@ for (i in 1:fold_number){
   new_training <- lou_training[train_index,]
   
   #decision tree model
-  lou.tree <- rpart(INCIDENT ~ PENALTY + VICTIM + LOCATION +COUNTRY + RESULT + GOALS + YEAR,data=new_training)
+  lou.tree <- rpart(INCIDENT ~ RESULT + GOALS + YEAR+MOON_NIGHT_BEFORE,data=new_training)
   pred.tree <- predict(lou.tree,newdata=new_testing,type="class")
   error.tree[i] <- mean(pred.tree != new_testing$INCIDENT)
   
-  #random forest model
-  lou.forest <- randomForest(INCIDENT ~ PENALTY + VICTIM + LOCATION +COUNTRY + RESULT + GOALS + YEAR,data=new_training)
-  pred.forest <- predict(lou.forest,newdata=new_testing,type="class")
-  error.forest[i] <- mean(pred.forest != new_testing$INCIDENT)
   
-  #lda model
-  lou.lda <- lda(INCIDENT ~ PENALTY + VICTIM + LOCATION +COUNTRY + RESULT + GOALS + YEAR,data=new_training)
-  pred.lda <- predict(lou.lda,newdata=new_testing)$class
-  error.lda[i] <- mean(pred.lda != new_testing$INCIDENT)
+  #decision tree model
+  lou.tree2 <- rpart(INCIDENT ~ PENALTY + VICTIM + LOCATION +WHEN+ RESULT + GOALS + YEAR+MOON_NIGHT_BEFORE,data=new_training)
+  pred.tree2 <- predict(lou.tree2,newdata=new_testing,type="class")
+  error.tree2[i] <- mean(pred.tree2 != new_testing$INCIDENT)
+ 
+  #decision tree model
+  lou.tree3 <- rpart(INCIDENT ~ PENALTY+LOCATION+WHEN,data=new_training)
+  pred.tree3 <- predict(lou.tree3,newdata=new_testing,type="class")
+  error.tree3[i] <- mean(pred.tree3 != new_testing$INCIDENT)
 }
 
 error.tree.mean <- mean(error.tree)
-error.forest.mean <- mean(error.forest)
-error.lda.mean <- mean(error.lda)
+error.tree2.mean <- mean(error.tree2)
+error.tree3.mean <- mean(error.tree3)
 
