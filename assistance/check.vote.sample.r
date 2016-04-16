0c9da9959ac87a9649dafb682c567c87b99b7447
Zhihan Fang <ezhihan@gmail.com>
2016-04-16 00:28:47 -0400

    check

---
+++ assistance/check.vote.sample.r
@@ -0,0 +1,12 @@
+check.vote.sample <- function(population.dist,candidate.names=NULL,sample.dist,loop.number){
+  if (length(population.dist) != length(sample.dist)){
+    print("sample distribution size shoud be identical to the population candidate number")
+    return(-1)
+  }
+  
+  
+  
+  
+}
+
+check.vote.sample(c(34,35,23),sample.dist = c(1,1),loop.number = 23)
